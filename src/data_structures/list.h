5a2ad702fe1b64b2ba5b8be34c5923ce038c891f
ruskeyz <lutayilya@gmail.com>
2023-09-08 15:59:36 +0100

    linked list implementation + tests"

--- src/data_structures/list.h
+++ src/data_structures/list.h
@@ -1,5 +1,5 @@
-#ifndef data_structures_List_h
-#define data_structures_List_h
+#ifndef _LIST_H_
+#define _LIST_H_
 
 #include <stdlib.h>
 
@@ -17,20 +17,38 @@ typedef struct List {
 } List;
 
 List *List_create();
+/** 
+ * Destroy List
+ */
 void List_destroy(List *list);
+/**
+ * Clears the values in the list, not the nodes
+ */
 void List_clear(List *list);
 void List_clear_destroy(List *list);
 
 #define List_count(A) ((A)->count)
 #define List_first(A) ((A)->first != NULL ? (A)->first->value: NULL)
 #define List_last(A) ((A)->last != NULL ? (A)->last->value: NULL)
-
+/**
+ * Push node to the end of the list
+ */
 void List_push(List *list, void *value);
+/**
+ * Take the last element off and return List
+ */
 void *List_pop(List *list);
-
+/**
+ * Add node to the front of the list 
+ */
 void List_unshift(List *list, void *value);
+/**
+ * Remove the first node element and return List
+ */
 void *List_shift(List *list);
-
+/**
+ * Remove elements from the list 
+ */
 void *List_remove(List *list, ListNode *node);
 
 #define LIST_FOREACH(L, S, M, V) ListNode *_node = NULL;\
--- src/dbg.h
+++ src/dbg.h
@@ -1,46 +1,39 @@
 #ifndef __dbg_h__
 #define __dbg_h__
 
-// http://c.learncodethehardway.org/book/ex20.html
-// http://en.wikipedia.org/wiki/Variadic_macro
-
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
 
 #ifdef NDEBUG
-// preprocessor will eliminate all debug() messages
 #define debug(M, ...)
 #else
-// log to stderr
-// Use a variadic macro to wrap printf and handle variable number of arguments.
-#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d:%s: " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define debug(M, ...) fprintf(stderr, "DEBUG %s:%d: " M "\n",\
+        __FILE__, __LINE__, ##__VA_ARGS__)
 #endif
 
-#define clean_errno() (0 == errno ? "None" : strerror(errno))
+#define clean_errno() (errno == 0 ? "None" : strerror(errno))
 
-// work like debug, but won't be ifdef'ed out
-// first two macros use clean_errno macro
-#define log_err(M, ...) fprintf(stderr, "[ERROR] (%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
-#define log_warn(M, ...) fprintf(stderr, "[WARN] (%s:%d:%s: errno: %s) " M "\n", __FILE__, __LINE__, __func__, clean_errno(), ##__VA_ARGS__)
-#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d:%s:) " M "\n", __FILE__, __LINE__, __func__, ##__VA_ARGS__)
+#define log_err(M, ...) fprintf(stderr,\
+        "[ERROR] (%s:%d: errno: %s) " M "\n", __FILE__, __LINE__,\
+        clean_errno(), ##__VA_ARGS__)
 
-// "best macro ever"
-// if A isn't true, log error and jump function's to error: label for cleanup
-// check macro uses log_err macro
-#define check(A, M, ...) if(!(A)) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+#define log_warn(M, ...) fprintf(stderr,\
+        "[WARN] (%s:%d: errno: %s) " M "\n",\
+        __FILE__, __LINE__, clean_errno(), ##__VA_ARGS__)
 
-// "second best macro ever"
-// put this in any part of a function that shouldn't run, such as in if or switch exceptional cases
-// logs error and jumps to function's to error: label for cleanup
-#define sentinel(M, ...) { log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+#define log_info(M, ...) fprintf(stderr, "[INFO] (%s:%d) " M "\n",\
+        __FILE__, __LINE__, ##__VA_ARGS__)
 
-// check pointer is valid
+#define check(A, M, ...) if(!(A)) {\
+    log_err(M, ##__VA_ARGS__); errno=0; goto error; }
+
+#define sentinel(M, ...)  { log_err(M, ##__VA_ARGS__);\
+    errno=0; goto error; }
+
 #define check_mem(A) check((A), "Out of memory.")
 
-// if not A, calls debug() and jumps to error: label
-// use with NDEBUG to skip debug print and still jump to error: label
-#define check_debug(A, M, ...) if (!(A)) { debug(M, ##__VA_ARGS__); errno=0; goto error; }
+#define check_debug(A, M, ...) if(!(A)) { debug(M, ##__VA_ARGS__);\
+    errno=0; goto error; }
 
 #endif
-
